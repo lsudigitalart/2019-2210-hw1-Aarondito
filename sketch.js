b26ad412953c92659336c48d3f8211fc1c6c3fc5
Aarondito <54543739+Aarondito@users.noreply.github.com>
2019-09-10 17:26:56 -0500

    made ellipse comp

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+createCanvas (600,800)   
+background(300)
+noFill()
+strokeWeight(5)
+ellipse(0,700,300,290)
+ellipse(700,0,300,290)
+ellipse(550,650,236,263)
+strokeWeight(9)
+ellipse(700,600,400,20)
+ellipse(700,0,400,100)
+ellipse(600,400,800,800)
+strokeWeight(13)
+ellipse(50, 50, 80, 80)
+  ellipse(100,100,500,500)
+  ellipse(100,70,70,100)
+  ellipse(100,100,75,75)
+  ellipse(400,500,90,72)
+  ellipse(450,550,80,50)
+  ellipse(300,320,40,40)
+  ellipse(300,300,20,20)
+  ellipse(200,100,45,60)
+  
+}
+
+function draw() {
+print(MouseX, MouseY)    
+  
+}
